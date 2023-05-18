267f6270c586dbbc4c0e79da58c51ba52e69e65d
David Martin <damartinco@gmail.com>
2023-05-18 10:21:41 -0400

    added info and work on list

--- PythonLearning/listPython.py
+++ PythonLearning/listPython.py
@@ -1,4 +1,18 @@
 # List are a essenstial Python Data Structure 
 # Below we will work on and dive into some of the basics of lists
 # Allows you to group values together under a common name
-dogs = ["Shiba", "Boarderdoodle", "Golden Retriever", "German Shepard"]
+dogs = ["Shiba", "Borderdoodle", "Golden Retriever", "German Sheperd"]
+
+# can mix multiple data types within a list
+new_dogs = ["Benny", 1, True, "Teddy", 12, False]
+# in operator to check if it is within the list
+print("Benny" in new_dogs)
+print("Rimuru" in new_dogs)
+
+# can also refrence based on index
+print(new_dogs[0]) # benny
+print(new_dogs[3]) # teddy
+
+# can also change the values within the list through the index
+dogs[0] = "Black Lab"
+print(dogs)
