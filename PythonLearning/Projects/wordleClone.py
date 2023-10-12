bfebc2fa8f9bdf9aa2fb1834b749eb861bcfb6af
David Martin <69823918+Marti746@users.noreply.github.com>
2023-10-12 13:38:15 -0600

    Update

--- PythonLearning/Projects/wordleClone.py
+++ PythonLearning/Projects/wordleClone.py
@@ -15,26 +15,33 @@ def game_instruction():
 
 # Function to check the user inputted word
 def check_word():
+    # Will grab a random word from the text file
     hidden_word = random.choice(words)
     attempt = 6
+    # While loop that loops through until there are no more guesses
     while attempt > 0:
+        # Converts guess to a str to compare
         guess = str(input("Guess the word: "))
         if guess == hidden_word:
             print("You guessed the words correctly! WIN 🕺🕺🕺 ")
             break
         else:
+            # will decrement the attempt and print out what is right and what is wrong in the word
             attempt = attempt - 1
             print(f"you have {attempt} attempt(s) ,, \n")
+            # Loops through the combined hidden word and the guess to see if any letter match
             for char, word in zip(hidden_word, guess):
+                # print if the letter is in the word and in the right spot
                 if word in hidden_word and word in char:
                     print(word + " ✔ ")
-
+                # Will print that the letter is in the word not right spot
                 elif word in hidden_word:
                     print(word + " ➕ ")
+                # will print if letter not in the word
                 else:
                     print(" ❌ ")
             if attempt == 0:
                 print(" Game over !!!! The word was: " + hidden_word)
-
+# calls the functions
 game_instruction()
 check_word()
